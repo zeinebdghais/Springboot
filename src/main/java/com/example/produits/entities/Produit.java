c9d03c224dffccf78f5ae7ce15bb0c2963f5b241
zeinebdghais <dghaiszeineb52@gmail.com>
2023-10-11 00:16:27 +0200

    first commit

---
+++ src/main/java/com/example/produits/entities/Produit.java
@@ -0,0 +1,82 @@
+package com.example.produits.entities;
+
+import java.util.Date;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Produit {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idProduit;
+	private String nomProduit;
+	private double prixProduit;
+	private Date dateCreation;
+	
+	@ManyToOne  //optionelle
+	private Categorie categorie;  //meme nom de mappedby dans la classe produit 
+	
+	public Produit() {
+		super();
+	}
+	
+	
+	public Produit(String nomProduit, double prixProduit, Date dateCreation) {
+		super();
+		this.nomProduit = nomProduit;
+		this.prixProduit = prixProduit;
+		this.dateCreation = dateCreation;
+	}
+
+
+	public long getIdProduit() {
+		return idProduit;
+	}
+	public void setIdProduit(long idProduit) {
+		this.idProduit = idProduit;
+	}
+	public String getNomProduit() {
+		return nomProduit;
+	}
+	public void setNomProduit(String nomProduit) {
+		this.nomProduit = nomProduit;
+	}
+	public double getPrixProduit() {
+		return prixProduit;
+	}
+	public void setPrixProduit(double prixProduit) {
+		this.prixProduit = prixProduit;
+	}
+	public Date getDateCreation() {
+		return dateCreation;
+	}
+	public void setDateCreation(Date dateCreation) {
+		this.dateCreation = dateCreation;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Produit [idProduit=" + idProduit + ", nomProduit=" + nomProduit + ", prixProduit=" + prixProduit
+				+ ", dateCreation=" + dateCreation + "]";
+	}
+
+
+	public Categorie getCategorie() {
+		return categorie;
+	}
+
+
+	public void setCategorie(Categorie categorie) {
+		this.categorie = categorie;
+	} 
+	
+
+}
